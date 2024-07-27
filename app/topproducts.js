0c351ccc7c178403f9e94af4cf704bba38fb211d
samiksha <samikshapattir21@navgurukul.org>
2024-07-27 12:37:24 +0530

    Top Products

--- app/topproducts.js
+++ app/topproducts.js
@@ -10,15 +10,59 @@ const TopProducts=()=>{
                     <div className="card-body">
                         <h2 className="card-title">Supplements</h2>
                         <p className="font-semibold">Rs 1000</p>
-                        <div className="card-action justify-end z-20">
-                            <button className="btn btn-circle btn-ghost">
-                                <i className="fa-solid fa-cart-plus"></i>
+                        <div className="card-action">
+                            <button className="btn add-card">
+                                <i class="fa-solid fa-cart-shopping"></i>
                             </button>
-                            <button className="btn btn-circle btn-ghost absolute top-0 right-0">
+                            <button className="btn like-btn ">
                                 <i className="fa-solid fa-heart" ></i>
                             </button>
                         </div>
-
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="image"></div>
+                    <div className="card-body">
+                        <h2 className="card-title">Supplements</h2>
+                        <p className="font-semibold">Rs 1000</p>
+                        <div className="card-action">
+                            <button className="btn add-card">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </button>
+                            <button className="btn like-btn ">
+                                <i className="fa-solid fa-heart" ></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="image"></div>
+                    <div className="card-body">
+                        <h2 className="card-title">Supplements</h2>
+                        <p className="font-semibold">Rs 1000</p>
+                        <div className="card-action">
+                            <button className="btn add-card">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </button>
+                            <button className="btn like-btn ">
+                                <i className="fa-solid fa-heart" ></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="image"></div>
+                    <div className="card-body">
+                        <h2 className="card-title">Supplements</h2>
+                        <p className="font-semibold">Rs 1000</p>
+                        <div className="card-action">
+                            <button className="btn add-card">
+                                <i class="fa-solid fa-cart-shopping"></i>
+                            </button>
+                            <button className="btn like-btn ">
+                                <i className="fa-solid fa-heart" ></i>
+                            </button>
+                        </div>
                     </div>
                 </div>  
             </div>
